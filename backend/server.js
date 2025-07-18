60e4fc9a92af0f83cdba5cedc2b026f53974a80f
Zarnigoor <zarnigorumaraliyeva8@gmail.com>
2025-07-18 11:24:44 -0700

    initial

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true,
+// }));
+
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 
