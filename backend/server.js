714724b265af5410a69086758a29b01881fb91b3
Zarnigoor <zarnigorumaraliyeva8@gmail.com>
2025-07-18 11:56:35 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,22 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://jekbek.vercel.app',
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'https://jekbek.vercel.app',
+//   credentials: true,
+// }));
+
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 200
+  })
+);
+
 
 app.use(cors())
 app.use(express.json());
